45cc37c41f93ab5cdccd398226865499c0288593
akmalays <akmalays@gmail.com>
2022-04-13 13:43:52 +0700

    add banner on top, mid and low section (homepage)

---
+++ components/midBanner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Image from "next/image";
+import banner3 from "../assets/Banner new Coming.png";
+
+function midBanner() {
+  return (
+    <div className="flex justify-end px-5">
+      <div className="max-w-4xl rounded ">
+        <Image src={banner3} className="rounded-xl" />
+      </div>
+    </div>
+  );
+}
+
+export default midBanner;
---
+++ components/topBanner.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Image from "next/image";
+import banner1 from "../assets/Group 45.png";
+import banner2 from "../assets/Group 46.png";
+
+function topBanner() {
+  return (
+    <div className="flex justify-end px-5">
+      <div className="flex items-end gap-2 max-w-4xl">
+        <Image src={banner1} />
+        <Image src={banner2} />
+      </div>
+    </div>
+  );
+}
+
+export default topBanner;
